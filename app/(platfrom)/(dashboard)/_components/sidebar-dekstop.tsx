6c023b39f67fd1233a9ca0c31d0181d8605c52c9
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-03 17:07:16 +0700

    feature: add card class

--- app/(platfrom)/(dashboard)/_components/sidebar-dekstop.tsx
+++ app/(platfrom)/(dashboard)/_components/sidebar-dekstop.tsx
@@ -1,6 +1,113 @@
 export const SidebarContent = () => {
   return (
-    <div className="max-h-full pe-4 hover:overflow-y-auto scrollbar scrollbar-track-background scrollbar-thumb-primary text-justify"></div>
+    <div className="max-h-full pe-4 hover:overflow-y-auto scrollbar scrollbar-track-background scrollbar-thumb-primary text-justify">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      consequuntur debitis porro dolores, quis expedita suscipit voluptas
+      laboriosam nulla illo cumque excepturi. Consequuntur beatae nesciunt et
+      nobis reiciendis recusandae sequi veniam, repellendus distinctio nam
+      voluptas ipsam odit labore libero dolorum facere, sed quis! Blanditiis ab
+      porro magni error repudiandae consectetur repellendus eum eaque, fugit
+      obcaecati vero doloribus tenetur neque ut. Cum voluptatum neque ab iure
+      labore rem corporis ut, quis earum maiores ducimus blanditiis doloribus,
+      dolorem sunt fuga. Odio non explicabo corrupti repudiandae architecto
+      vitae, nesciunt hic libero harum illo aspernatur recusandae aliquid quae
+      dolore labore minima fugit nisi molestiae similique aliquam maiores? Hic
+      in autem mollitia, eos impedit omnis est ad magni architecto ea iste unde
+      voluptate laudantium, iusto quae adipisci temporibus at veniam? Cum ipsam
+      esse, consequatur aperiam maiores doloribus mollitia illo aliquam
+      accusamus. Iste corporis eaque quisquam eum id quae repellat porro, fugiat
+      voluptatum repellendus veritatis beatae inventore tenetur hic rerum iure
+      neque ab perferendis delectus distinctio ullam pariatur dolorem. Odit amet
+      adipisci sed, laborum, eaque quis aspernatur deleniti illo totam, velit
+      recusandae facere quasi! Laudantium, dicta suscipit, ratione molestias
+      eius autem accusamus omnis placeat obcaecati voluptatum inventore! Ab
+      expedita, eveniet reprehenderit molestias fuga vitae nemo tempora facere
+      officia accusantium, quae dicta voluptatem quasi? Fuga dolor quasi nostrum
+      accusamus ut error eos, consequuntur aliquam alias nesciunt excepturi ea
+      commodi deleniti laboriosam minus earum voluptatibus. Labore autem beatae
+      dolorum amet expedita repudiandae, natus blanditiis provident dolorem sed
+      quis iste, cumque tenetur sit officiis. Nihil, fugit atque quas
+      repellendus libero facere quibusdam obcaecati nesciunt veritatis nobis.
+      Quas vitae sit dolor ipsa magni pariatur. Pariatur, architecto.
+      Consectetur rerum beatae modi accusamus nesciunt quod, ex veritatis
+      provident. Recusandae ipsa nobis deserunt temporibus veritatis quod
+      quaerat. Fugit quo rem officiis cupiditate illo molestias doloribus, vel
+      temporibus veniam molestiae, vero eligendi incidunt soluta fugiat nihil
+      eius accusantium nisi natus impedit reiciendis. Modi rem natus quaerat,
+      tempore cum, commodi et numquam temporibus inventore, rerum laudantium
+      nostrum quis error perspiciatis? Laborum impedit reiciendis molestias
+      aperiam consectetur eum iusto accusantium doloribus velit quam id fugiat
+      eius fugit consequuntur voluptatibus inventore expedita, officia a,
+      explicabo est laudantium. Velit molestias labore debitis eos et dolor amet
+      eaque recusandae totam fugiat, qui eum veniam laboriosam, sequi assumenda
+      fugit numquam. Doloremque aliquam rem esse vitae sequi voluptas aliquid,
+      provident iste culpa, fuga voluptate vero corrupti reiciendis sunt totam
+      quo minus accusamus at commodi in officiis nihil! Maiores repellendus
+      animi, vitae blanditiis autem minima. Reprehenderit eum recusandae sunt
+      ipsum sint odio. Ipsum aspernatur assumenda facilis voluptatem corporis
+      inventore harum laudantium, soluta eveniet libero odit, illum praesentium
+      obcaecati! Obcaecati nemo ullam nulla repellendus odio, harum quia sunt
+      error soluta. Assumenda debitis quis excepturi sapiente dolorem iure
+      maiores tenetur amet neque, modi velit soluta veniam provident quo
+      voluptatibus accusantium ab ad ex quae officiis obcaecati. Cumque repellat
+      ullam esse illum atque minima nemo veritatis in et quasi, totam
+      consequuntur eius velit pariatur ut sequi vitae ea labore illo id
+      voluptatem aut non voluptatibus. Omnis amet a impedit aut, error nobis
+      mollitia ut rerum delectus, quod cumque. Nam quam iste a porro laudantium
+      sed. Eaque tempora, error neque voluptates praesentium recusandae
+      consequuntur, tempore in quia temporibus impedit eum veniam veritatis ut
+      dolore debitis quibusdam fugiat provident numquam eveniet dolorem, quidem
+      cumque ducimus. Voluptates, quis natus cum ad consectetur eius harum
+      mollitia culpa incidunt libero reiciendis, doloremque deleniti nesciunt
+      saepe. Vitae fugit architecto maxime repellendus doloribus eveniet
+      dolorum! Ea, distinctio. Possimus deleniti error facere modi accusamus cum
+      temporibus recusandae delectus doloremque suscipit deserunt ipsa
+      praesentium necessitatibus, reiciendis impedit minus animi, nostrum
+      distinctio fugiat, maxime amet ut vel autem. Et doloremque incidunt quod
+      velit praesentium corporis ut magnam nam voluptas ducimus eveniet error
+      perspiciatis asperiores rerum dolorum est iusto hic, debitis suscipit vel
+      eligendi unde obcaecati magni! Natus minus libero vero? Eos dignissimos
+      molestias officia, aliquam accusantium iste laudantium deserunt possimus
+      excepturi natus molestiae quaerat aspernatur nobis, voluptatem non porro,
+      voluptas soluta fugit debitis! Provident, at. Magnam dolor, vitae maiores
+      cum consequuntur obcaecati libero cupiditate aspernatur eaque accusantium
+      consectetur iusto quis. Pariatur porro animi atque distinctio, explicabo
+      nesciunt ipsum eum voluptas nihil officia quasi neque, nam consequatur
+      vero quas deleniti similique mollitia dignissimos minima exercitationem
+      tempore numquam est delectus iure. Iusto quae odio assumenda asperiores
+      consequatur culpa dolore fuga dolorem, minus dignissimos fugit libero
+      nemo? Adipisci voluptatum eveniet neque placeat quos labore maxime amet
+      eum praesentium laudantium sint pariatur iure minus quia, consequatur
+      velit beatae molestiae cumque. Eos perspiciatis, soluta, minus quos sequi
+      eveniet voluptate et consequatur ipsa officia nam! Deleniti, sequi natus
+      facere incidunt alias dicta ad ea ex soluta consequatur unde excepturi
+      placeat praesentium deserunt consequuntur vero corrupti ducimus officiis
+      iste? Nobis, nisi, itaque quae tenetur asperiores dolores esse consequatur
+      cumque tempore doloremque soluta sunt aperiam sequi fuga sit nemo placeat
+      eveniet fugit ratione odio velit veniam id. Illo tempore nesciunt
+      perferendis nobis dolore illum sint magnam eaque doloribus pariatur
+      suscipit esse commodi totam aspernatur consectetur harum consequuntur
+      saepe repellat expedita, numquam veritatis tempora neque et? Nesciunt
+      repellat eius, provident in ab consectetur blanditiis enim iure iste!
+      Accusantium odio quibusdam dicta saepe cum maxime omnis rem aspernatur
+      voluptates nihil perspiciatis ratione, porro laborum repellat! Hic officia
+      mollitia sapiente eius magni soluta porro esse dignissimos sed inventore
+      quaerat consequuntur iusto error fugit pariatur facilis, ullam doloremque
+      impedit consequatur at aspernatur numquam quos? Impedit fuga labore,
+      aspernatur accusantium dolorum ad modi velit nihil pariatur repellendus
+      est facilis voluptatibus dolorem culpa quam itaque quae! Pariatur commodi
+      ad, quia aliquid molestiae explicabo ea quas iste porro? Ex provident
+      dolor perferendis quidem. Enim ab autem fuga eum. Similique explicabo
+      consectetur quae voluptates, quasi, autem, eligendi veritatis laboriosam
+      numquam rem enim? Fuga, alias esse. Distinctio incidunt exercitationem
+      accusamus at facilis quae amet, velit a dolorum. Nobis ut sunt natus
+      dignissimos voluptatum dolorem maiores modi cum fugiat ducimus animi
+      cumque nihil sed, incidunt minima ullam voluptate reprehenderit enim
+      soluta voluptates perspiciatis exercitationem similique deserunt autem.
+      Voluptate in delectus officiis ducimus placeat accusantium, mollitia quas
+      dolores? Hic nesciunt quis possimus corporis dolorum, explicabo adipisci
+      quisquam esse expedita officiis eum, error quas beatae!
+    </div>
   );
 };
 
--- app/(platfrom)/(dashboard)/app/page.tsx
+++ app/(platfrom)/(dashboard)/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Meteors } from "@/components/ui/ui-aceternity/meteors";
+import { TextGenerateEffect } from "@/components/ui/ui-aceternity/text-generate-effect";
 import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata() {
   const user = await currentUser();
@@ -9,8 +15,73 @@ export async function generateMetadata() {
   };
 }
 
+// img : https://www.gstatic.com/classroom/themes/img_code.jpg
+
 const PlatformMainPage = async () => {
-  return <div className="pt-16 px-5 text-justify"></div>;
+  return (
+    <>
+      <div className="pt-16 px-5 w-full">
+        <div className="flex justify-between items-center">
+          <TextGenerateEffect
+            className="mb-5 text-sm md:text-3xl xl:text-5xl font-extrabold"
+            words="Daftar Kelas"
+          />
+          <div>
+            <Button
+              title="Tambah kelas"
+              variant="outline"
+              className="border px-4 py-1 rounded-lg"
+            >
+              <Plus className="w-5 h-5" />
+            </Button>
+          </div>
+        </div>
+
+        <div className="card-container">
+          {Array.from({ length: 20 }, (_, i) => (
+            <div
+              key={i}
+              className="w-full relative z-10 flex flex-col rounded-2xl shadow-sm bg-bacground border border-primary-800  h-full overflow-hidden"
+            >
+              <img
+                src="https://www.gstatic.com/classroom/themes/img_code.jpg"
+                alt=""
+                className="h-full"
+              />
+              <div className="relative  p-4 flex flex-col justify-end items-start">
+                <h1 className="font-bold text-xl text-foreground relative z-50 line-clamp-2">
+                  Pemrograman Platform Web
+                </h1>
+                <p className=" mb-4 text-muted-foreground text-sm line-clamp-1">
+                  Lorem Ipsum Dolor, S.IP, M.Si., Ph.D
+                </p>
+
+                <p className="font-normal text-base text-muted-foreground mb-4 relative z-50 text-justify  line-clamp-3">
+                  I don&apos;t know what to write so I&apos;ll just paste
+                  something cool here. One more sentence because lorem ipsum is
+                  just unacceptable. Won&apos;t ChatGPT the shit out of this.
+                </p>
+
+                <div className="flex">
+                  <Button
+                    variant="default"
+                    className="border px-4 py-1 rounded-lg"
+                  >
+                    <Link href={`/app/class/${i + 1}`}>
+                      Lihat catatan tugas
+                    </Link>
+                  </Button>
+                </div>
+
+                {/* Meaty part - Meteor effect */}
+                <Meteors number={5} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PlatformMainPage;
