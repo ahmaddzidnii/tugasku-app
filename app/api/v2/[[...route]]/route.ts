7c5d762f77d62409c235017cabc7bf50025c8a4f
ahmaddzidnii <zdngaming312@gmail.com>
2024-05-15 13:41:47 +0700

    refactor: menulis ulang kode untuk api classes menggunakan hono js

--- app/api/v2/[[...route]]/route.ts
+++ app/api/v2/[[...route]]/route.ts
@@ -1,32 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
-
-export const runtime = "edge";
+import classes from "./classes";
+export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api/v2");
 
-app
-  .get("/hello", (c) => {
-    return c.json({
-      message: "Hello Next.js!",
-    });
-  })
-  .get(
-    "/hello/:name",
-    zValidator("param", z.object({ name: z.string() })),
-    zValidator("query", z.object({ age: z.string().optional() })),
-    (c) => {
-      const { name } = c.req.valid("param");
-      const { age } = c.req.valid("query");
-      return c.json({
-        message: `Hello ${name})}!`,
-        age,
-      });
-    }
-  );
+app.route("/classes", classes);
 
 export const GET = handle(app);
 export const POST = handle(app);
