e4cb0d15c56f198501ee699541a709170fad164a
ahmaddzidnii <zdngaming312@gmail.com>
2024-05-24 13:44:11 +0700

    refactor(api): typo schema

--- app/api/schema/assignments/update.ts
+++ app/api/schema/assignments/update.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const UpdateAssignment = z.object({
-  asignmentTitle: z.string().optional(),
-  asignmentDescription: z.string().optional(),
+  assignmentTitle: z.string().optional(),
+  assignmentDescription: z.string().optional(),
   dueDate: z.string().optional().nullable(),
   isCompleted: z.boolean().optional(),
 });
